9aeb2c86234b0fe9e14b7b05590551d0d41be166
francoheredev <franco_mapa@outlook.com>
2026-06-18 20:47:48 -0300

    UN POQUITO DE TOQUES SOLAMENTE

--- scripts/menú.gd
+++ scripts/menú.gd
@@ -3,7 +3,7 @@ extends Control
 
 func _process(_delta):
 
-	krits_label.text = "Krits: " + str(GameManager.krits)
+	krits_label.text = ": " + str(GameManager.krits)
 func _on_jugar_button_pressed():
 	get_tree().change_scene_to_file("res://escenas/gameplay.tscn")
 
